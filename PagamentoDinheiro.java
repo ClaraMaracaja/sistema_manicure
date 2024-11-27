0f4b7650d5f339e2ac8e5dc7210e02da27b67610
lararf05 <164400685+lararf05@users.noreply.github.com>
2024-11-27 14:55:07 -0300

    Add files via upload

---
+++ PagamentoDinheiro.java
@@ -0,0 +1,16 @@
+public class PagamentoDinheiro implements Pagamento {
+    private double valorPago;
+
+
+    @Override
+    public void realizarPagamento(double valor) {
+        this.valorPago = valor;
+        System.out.println("Pagamento de R$ " + valor + " realizado em dinheiro.");
+    }
+
+    @Override
+    public String obterDetalhesPagamento() {
+        return "Pagamento em dinheiro: R$ " + valorPago;
+
+    }
+}
